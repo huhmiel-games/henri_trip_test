69bda7f9642fa3ac8143d15cb36974b6417be09f
philippe Pereira <phil.pereira@protonmail.com>
2024-09-02 14:52:05 +0200

    routes are now protected by authorizations

--- henry_trip_api/start/routes.ts
+++ henry_trip_api/start/routes.ts
@@ -8,9 +8,46 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import UsersController from '../app/controllers/users_controller.js'
+import GuidesController from '../app/controllers/guides_controller.js'
+import ActivitiesController from '../app/controllers/activities_controller.js'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+router.get('/', async () =>
+{
+    return {
+        hello: 'hello henri trip api',
+    }
 })
+
+// user
+router.group(() =>
+{
+    router.post('users/register', [UsersController, 'register'])
+    router.post('users/login', [UsersController, 'login'])
+})
+    .prefix('/api')
+
+// admin routes
+router.group(() =>
+{
+    router.get('users', [UsersController, 'all'])
+    router.get('guides', [GuidesController, 'all'])
+    router.post('guides', [GuidesController, 'create'])
+    router.post('guides/schedule', [GuidesController, 'addSchedule'])
+    router.get('activity', [ActivitiesController, 'all'])
+    router.post('activity', [ActivitiesController, 'create'])
+    router.post('activity/openinghours', [ActivitiesController, 'addOpeningHours'])
+})
+    .use([middleware.auth(), middleware.userIsAdmin()])
+    .prefix('/api')
+
+// authenticated user routes
+router.group(() =>
+{
+    router.get('guides/:id', [GuidesController, 'find'])
+    router.get('activity/:id', [ActivitiesController, 'find'])
+})
+    .use(middleware.auth())
+    .prefix('/api')
+
