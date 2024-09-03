2335ee2eb2643266ef6367d7fde0736a16a176e9
philippe Pereira <phil.pereira@protonmail.com>
2024-09-03 18:34:42 +0200

    ready to use env and database

---
+++ henry_trip_api/tmp/default_queries/populate.sql
@@ -0,0 +1,38 @@
+-- SQLite
+INSERT INTO mobilities (name)
+VALUES ('voiture');
+INSERT INTO mobilities (name)
+VALUES ('vélo');
+INSERT INTO mobilities (name)
+VALUES ('à pied');
+INSERT INTO mobilities (name)
+VALUES ('moto');
+
+INSERT INTO for_whoms (name)
+VALUES ('famille');
+INSERT INTO for_whoms (name)
+VALUES ('seul');
+INSERT INTO for_whoms (name)
+VALUES ('en groupe');
+INSERT INTO for_whoms (name)
+VALUES ('entre amis');
+
+INSERT INTO seasons (name)
+VALUES ('printemps');
+INSERT INTO seasons (name)
+VALUES ('été');
+INSERT INTO seasons (name)
+VALUES ('automne');
+INSERT INTO seasons (name)
+VALUES ('hiver');
+
+INSERT INTO categories (name)
+VALUES ('musée');
+INSERT INTO categories (name)
+VALUES ('château');
+INSERT INTO categories (name)
+VALUES ('activité');
+INSERT INTO categories (name)
+VALUES ('parc');
+INSERT INTO categories (name)
+VALUES ('grotte');
